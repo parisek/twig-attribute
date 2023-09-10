a5928a487a41ae6d45e340d7351ec37ed2c7d334
Petr Parimucha <petr@pari.cz>
2023-09-10 14:16:09 +0200

    Attribute class update (#1)
    Update based on https://www.drupal.org/project/drupal/issues/2664570
    
    PHP 8.1+ required

--- AttributeExtension.php
+++ AttributeExtension.php
@@ -2,29 +2,26 @@
 
 namespace Parisek\Twig;
 
-use Drupal\Component\Attribute\AttributeCollection;
+use Drupal\Core\Template\AttributeCollection;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFunction;
 use Twig\Environment;
 
-class AttributeExtension extends AbstractExtension
-{
-    public function getFunctions(): array
-    {
-        return [
-            new TwigFunction(
-                "create_attribute",
-                [$this, "createAttribute"],
-                [
-                    "needs_environment" => true,
-                    "is_safe" => ["html"],
-                ]
-            ),
-        ];
-    }
+class AttributeExtension extends AbstractExtension {
+  public function getFunctions(): array {
+    return [
+      new TwigFunction(
+        "create_attribute",
+        [$this, "createAttribute"],
+        [
+          "needs_environment" => true,
+          "is_safe" => ["html"],
+        ]
+      ),
+    ];
+  }
 
-    public function createAttribute(Environment $environment, array $attributes = []): object
-    {
-        return new AttributeCollection($attributes);
-    }
+  public function createAttribute(Environment $environment, array $attributes = []): object {
+    return new AttributeCollection($attributes);
+  }
 }
